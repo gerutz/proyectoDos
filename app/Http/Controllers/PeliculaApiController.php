496b034850bb44226b825fb00acc34cbd4198a08
gerutz <germanutz@gmail.com>
2016-07-06 20:45:47 -0300

    proyecto funcionando

--- app/Http/Controllers/PeliculaApiController.php
+++ app/Http/Controllers/PeliculaApiController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Pelicula;
-use Dingo\Api\Contract\Routing\Helpers;
+use Dingo\Api\Routing\Helpers;
 use App\Http\Requests;
+use App\Http\Transformers\PeliculaTransformer;
 
 class PeliculaApiController extends Controller
 {
     use Helpers;
 
     public function listado(){
+
         $pelicula = Pelicula::all();
 
-        return $this->response->collection($pelicula);
+        return $this->response->collection($pelicula, new PeliculaTransformer());
+
     }
 }
--- app/Http/Transformers/PeliculaTransformer.php
+++ app/Http/Transformers/PeliculaTransformer.php
@@ -4,15 +4,16 @@ namespace App\Http\Transformers;
 use App\Pelicula;
 use League\Fractal\TransformerAbstract;
 
-class PeliculaTransfomer extends TransformerAbstract {
+class PeliculaTransformer extends TransformerAbstract {
 
     public function transform(Pelicula $pelicula){
+
+
         return [
             'id'     => $pelicula->id,
             'title'  => $pelicula->titulo,
             'rating' => $pelicula->rating,
-            'length' => $pelicula->duracion,
-            'genre'  => $pelicula->genero->nombre
+            'length' => $pelicula->duracion 
         ];
     }
 }
